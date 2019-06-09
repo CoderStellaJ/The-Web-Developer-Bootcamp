d85b55518c58f236d1d0f01e05387a55f0b653fe
CoderStellaJ <40347063+CoderStellaJ@users.noreply.github.com>
2019-06-09 11:04:37 +0800

    html and css
    first commit of HTML and CSS part

---
+++ Exercises_JS/Guess a Number/script.js
@@ -0,0 +1,5 @@
+var number = Number(prompt("Please guess a number ~"));
+while(number != 6){
+	number = Number(prompt("Please guess a number ~"));
+}
+alert("You got it right!");
---
+++ Exercises_JS/JS_Basics/script.js
@@ -0,0 +1,6 @@
+var firstname = prompt("what's your first name?");
+var lastname = prompt("what's your last name?");
+var myage = prompt("what's your age?");
+console.log("Your name is "+firstname+" "+lastname);
+// each log for a line
+console.log("Your age is "+myage);
